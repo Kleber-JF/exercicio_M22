5d93f924c7afb0e89f6ddc3313a7d00d14ff2759
Kleber Fukushima <kleber.jhun@gmail.com>
2023-08-22 14:34:05 -0300

    site completo

---
+++ src/scripts/main.js
@@ -0,0 +1,25 @@
+AOS.init();
+document.addEventListener('DOMContentLoaded',function (){
+    const eventDate = new Date("Dec 12, 2023 19:00:00");
+    const timeStampEvent = eventDate.getTime();
+    
+    const countdown = setInterval(function(){
+        const now = new Date();
+        const timeStampNow = now.getTime();
+    
+        const timeToEvent = timeStampEvent - timeStampNow;
+    
+        const daysToEvent = Math.floor(timeToEvent / (1000*60*60*24));
+        const hoursToEvent = Math.floor(timeToEvent % (1000*60*60*24)/(1000*60*60));
+        const minToEvent = Math.floor(timeToEvent % (1000*60*60)/(1000*60));
+        const secToEvent = Math.floor(timeToEvent % (1000*60)/(1000));
+    
+        document.getElementById('contador').textContent = `${daysToEvent} dias ${hoursToEvent}:${minToEvent}:${secToEvent}`
+    
+        if (timeToEvent < 0) {
+            clearInterval(countdown);
+            document.getElementById('contador').textContent = 'Evento expirado'
+        }
+    }, 1000)
+
+})
